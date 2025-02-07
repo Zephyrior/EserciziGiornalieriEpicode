c0a2c2d5b04dad178c3838cb0c72fb6159215225
Zephyrior <mark.temblique@gmail.com>
2025-02-07 18:03:53 +0100

    pusho

---
+++ S9:L1/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from "react";
+//import reactLogo from "./assets/react.svg";
+//import viteLogo from "/vite.svg";
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+
+function App() {
+  return (
+    <>
+      <ButtonComponent btnText="button text" />
+    </>
+  );
+}
+
+export default App;
---
+++ S9:L1/my-react-app/src/components/ButtonComponent.jsx
@@ -0,0 +1,5 @@
+const ButtonComponent = (props) => {
+  console.log(props);
+  return <button>{props.btnText}</button>;
+};
+export default ButtonComponent;
